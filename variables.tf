d065b559ccfec349cc740332d2949bdead2fcbbe
51r <petar.spasov@hashicorp.com>
2022-06-20 09:18:09 +0300

    modified the var descriptions

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "email" {
 
 variable "instances" {
   type        = string
-  description = "Specify how much instances should be build, this number should be odd (2,4,6..)"
+  description = "Specify how much instances should be build, this number should be even (2,4,6..)"
   default     = "6"
 }
 
